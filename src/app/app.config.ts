4e6390e7e439277ca733d56ad681f585910108c9
tuhdanger002 <evg.wilfredbelliard@gmail.com>
2026-02-09 22:52:50 -0400

    adaptando espanol

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -17,6 +17,45 @@ export const appConfig: ApplicationConfig = {
                 options: {
                     darkModeSelector: 'always'
                 }
+            },
+            translation: {
+                startsWith: 'Empieza con',
+                contains: 'Contiene',
+                notContains: 'No contiene',
+                endsWith: 'Termina con',
+                equals: 'Igual a',
+                notEquals: 'No igual a',
+                noFilter: 'Sin filtro',
+                lt: 'Menor que',
+                lte: 'Menor o igual que',
+                gt: 'Mayor que',
+                gte: 'Mayor o igual que',
+                is: 'Es',
+                isNot: 'No es',
+                before: 'Antes de',
+                after: 'Después de',
+                dateIs: 'Fecha es',
+                dateIsNot: 'Fecha no es',
+                dateBefore: 'Fecha antes de',
+                dateAfter: 'Fecha después de',
+                clear: 'Limpiar',
+                apply: 'Aplicar',
+                matchAll: 'Coincidir con todos',
+                matchAny: 'Coincidir con cualquiera',
+                addRule: 'Agregar regla',
+                removeRule: 'Eliminar regla',
+                accept: 'Aceptar',
+                reject: 'Rechazar',
+                choose: 'Elegir',
+                upload: 'Subir',
+                cancel: 'Cancelar',
+                dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+                dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
+                dayNamesMin: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
+                monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+                monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+                today: 'Hoy',
+                weekHeader: 'Sem'
             }
         })
     ]
